06d4b202104433b9b4afcdce9f688fa915c82784
pranav-05 <70014211+pranav-05@users.noreply.github.com>
2020-10-28 14:26:07 +0530

    Float to absolute committed

---
+++ Conversion of float  to absolute value .c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+	int i;
+	double interval,a,b;
+	for(i=0;i<60;i++)
+	{
+		a=sin(interval);
+		b=fabs(a); //for integers we use abs() and for floting nos use fabs()...
+		interval=i/10.0;
+	    printf("sin(%lf)=%f\t",interval,a);
+		printf("abs(%lf)=%f\n",a,b);
+		
+	}
+	printf("\n+++++++\n");
+	return 0;
+}
+
+ 
+   
